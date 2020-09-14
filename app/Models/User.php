10141a8c82f725680c22b2aae7365fb0e4c02668
luis <valdivialuis1989@gmail.com>
2020-09-13 19:07:30 -0500

    user list

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,9 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+
+    protected $connection = "mysql_2";
+
     /**
      * The attributes that are mass assignable.
      *
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+
+Route::get("user",["as"=>"list_user","uses"=>"App\Http\Controllers\UserController@list"]);
